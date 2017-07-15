20c8c888fe1f1cd0c486acac8b2ecff18af18ff3
thomasyimgit <thomasyim94@gmail.com>
2017-07-16 00:16:06 +0800

    components & styles: well => wellGrid => SquareBlocks

---
+++ src/components/SquareBlock.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SquareBlock = ({ color }) => (
+  <div className="square-block" style={{ backgroundColor: color }}></div>
+)
+
+export default SquareBlock
--- src/components/Well.js
+++ src/components/Well.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import WellGrid from './WellGrid'
 import Tetromino from './Tetromino'
 
 import './styles/Well.css'
 
-export default class Well extends Component {
+const shit = [[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, "#b04497", null, null, null, null],
+[null, null, null, null, "#b04497", "#b04497", "#b04497", null, null, null],
+[null, null, null, null, null, "#ed652f", null, null, null, null],
+[null, null, null, "#ed652f", "#ed652f", "#ed652f", null, null, null, null]]
+
+class Well extends Component {
+  getWellGridProps() {
+
+  }
+
   render() {
+    const { grid } = this.props
     return (
       <div className="well-container">
-        <WellGrid />
+        <WellGrid grid={ shit } />
         {/*<Tetromino />*/}
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    grid: state.grid
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Well)
