6281ba19115c7e786be127fb79610dcd9a4fe931
thomasyimgit <thomasyim94@gmail.com>
2017-10-14 20:46:32 -0400

    fix test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Well } from './components/Well';
+import { Well } from '../components/Well';
 // http://cn.redux.js.org/docs/recipes/WritingTests.html
 
 it('renders without crashing', () => {
